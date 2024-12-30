a52e445156c0dad930a39c77053f97a83f69eaea
Rakib Islam <109210735+Koragg98@users.noreply.github.com>
2024-12-30 17:41:15 +0530

    Create UnionOfArrays.java

---
+++ Hashing/UnionOfArrays.java
@@ -0,0 +1,17 @@
+/*
+Prob.: Union of Arrays with Duplicates
+Given two arrays a[] and b[], the task is to find the number of elements in the union between these two arrays.
+The Union of the two arrays can be defined as the set containing distinct elements from both arrays. 
+If there are repetitions, then only one element occurrence should be there in the union.
+Note: Elements of a[] and b[] are not necessarily distinct.
+*/
+
+class Solution {
+    public static int findUnion(int a[], int b[]) {
+        // code here
+        HashSet<Integer> hs=new HashSet<>();
+        for(int x:a) hs.add(x);
+        for(int y:b) hs.add(y);
+        return hs.size();
+    }
+}
