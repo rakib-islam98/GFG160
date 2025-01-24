54b4a4a6c827da8707ced733ca7bcd8e1542f385
Rakib Islam <109210735+Koragg98@users.noreply.github.com>
2025-01-24 16:55:49 +0530

    Create DetectLoop.java

---
+++ Linked List/DetectLoop.java
@@ -0,0 +1,27 @@
+
+
+/*
+Prob.: Detect Loop in linked list
+You are given the head of a singly linked list. Your task is to determine if the linked list contains a loop.
+A loop exists in a linked list if the next pointer of the last node points to any other node in the list (including itself), rather than being null.
+
+Custom Input format:
+A head of a singly linked list and a pos (1-based index) which denotes the position of the node to which the last node points to.
+If pos = 0, it means the last node points to null, indicating there is no loop.
+*/
+
+class Solution {
+    // Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head) {
+        // Add code here
+        if(head==null) return false;
+        Node fast=head,slow=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
